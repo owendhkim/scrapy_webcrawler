c03e30a549770bb73d76cb4955f0104e5e26cb80
Owen Kim <cvm_owenk@iastate.edu>
2024-06-05 17:48:35 -0500

    pagination feature done, query params added

--- downloads/apache-nutch-1.20/site/server.js
+++ downloads/apache-nutch-1.20/site/server.js
@@ -1,6 +1,7 @@
 const SolrNode = require('solr-node');
 const express = require('express');
 const path = require('path');
+const { title } = require('process');
 const app = express();
 const port = 5000;
 
@@ -21,13 +22,24 @@ app.get('/', (req, res) => {
 
 app.get('/search', (req, res) => {
     const userQuery = req.query.q; // Get the user query from the URL parameter
+    const page = req.query.page ? parseInt(req.query.page) : 1; // Get the page number from the URL parameter
+    const resultsPerPage = 10;
+    const start = (page - 1) * resultsPerPage;
+
     if (!userQuery) {
         return res.status(400).send('Search query is required');
     }
     const formattedQuery = `"${userQuery}"`;
+    var params = [
+        { field: 'defType', value: 'edismax' },
+        { field: 'q.op', value: 'AND' },
+        { field: 'q', value: userQuery },
+        { field: 'qf', value: 'content title^2' }
+      ];
     const solrQuery = client.query()
-                        .q({content:formattedQuery})
-                        .rows(10000);
+                        .start(start)
+                        .rows(resultsPerPage)
+                        .addParams(params);
     client.search(solrQuery, (err, result) => {
         if (err) {
             console.error('Solr error:', err);
